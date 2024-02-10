d042467825b47d49e883f5d62fb6d8b762d487fb
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-09 19:28:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
 provider "google" {
-  project = var.project_id
+  project = "<your-project-id>"
+  region  = "us-central1"
 }
 
-module "storage_buckets" {
-  source  = "terraform-google-modules/cloud-storage/google"
+module "gcs_buckets" {
+  source = "terraform-google-modules/cloud-storage/google//modules/buckets"
   version = "~> 3.0"
 
-  buckets = var.bucket_names
+  for_each = toset(["bucket-1", "bucket-2", "bucket-3", "bucket-4", "bucket-5"])
+
+  name = "${each.value}-${format("%02d", index(tolist(keys(var.buckets)), each.value))}"
+}
+
+output "bucket_urls" {
+  value = values(module.gcs_buckets)[*].url
 }
