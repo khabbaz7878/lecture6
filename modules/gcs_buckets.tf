ef763e637c40eb7bc5f766f16864e549ac0d7fa1
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-09 19:34:35 -0500

    Update gcs_buckets.tf

--- modules/gcs_buckets.tf
+++ modules/gcs_buckets.tf
@@ -1,1 +1,9 @@
 
+module "gcs_buckets" {
+  source = "terraform-google-modules/cloud-storage/google//modules/buckets"
+  version = "~> 3.0"
+
+  for_each = toset(["bucket-1", "bucket-2", "bucket-3", "bucket-4", "bucket-5"])
+
+  name = "${each.value}-${format("%02d", index(tolist(keys(var.buckets)), each.value))}"
+}
