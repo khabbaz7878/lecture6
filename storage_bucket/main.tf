a1b845836206623969cbad1fa2bf35ea157476e1
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-09 20:43:05 -0500

    Update main.tf

--- storage_bucket/main.tf
+++ storage_bucket/main.tf
@@ -2,6 +2,12 @@
 resource "google_storage_bucket" "bucket" {
   name     = var.bucket_name
   location = var.location
+module storage_bucket {
+  source       = "./storage_bucket"
+  project_id   = "speedy-toolbox-360600"
+  region       = "us-central1"
+  bucket_names = ["bucket1", "bucket2", "bucket3", "bucket4", "bucket5"]
+}
 
   # Add other bucket configurations as needed
 }
