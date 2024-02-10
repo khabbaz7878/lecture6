f6b060c18db9a348b896bd25988c953a66c3ad06
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-09 20:52:47 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
-
 module "storage_buckets" {
   source = "./storage_bucket"
 
-  for_each = toset(["1", "2", "3", "4", "5"]) // Set of bucket names
-  bucket_name = "my-bucket-${each.key}"
-}
-
-output "bucket_names" {
-  value = [for bucket_key, bucket in module.storage_buckets : bucket.name]
+  bucket_name_suffix = ["1", "2", "3", "4", "5"]
 }
