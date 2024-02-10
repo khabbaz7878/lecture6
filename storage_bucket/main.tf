a812f147f24cddee796781e8745c92a8161677f1
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-09 20:56:17 -0500

    Update main.tf

--- storage_bucket/main.tf
+++ storage_bucket/main.tf
@@ -1,11 +1,4 @@
-variable "bucket_name_suffix" {
-  description = "Suffix for the storage bucket name"
-}
 
-variable "location" {
-  description = "Location for the storage bucket"
-  default     = "US"
-}
 
 resource "google_storage_bucket" "bucket" {
   name     = "my-bucket-${var.bucket_name_suffix}"
