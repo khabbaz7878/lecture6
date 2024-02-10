6e23eb4491a407dd88358aa7a5320de3b1beae9f
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-09 19:38:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,24 @@ module "gcs_buckets" {
 output "bucket_urls" {
   value = values(module.gcs_buckets)[*].url
 }
+
+
+
+# Configure the Google Cloud provider
+provider "google" {
+  project = "<speedy-toolbox-360600>"
+  region  = "us-central1"
+}
+
+# Create 5 bucket modules
+module "bucket" {
+  source  = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
+  version = "~> 1.7"
+
+  for_each = toset(["1", "2", "3", "4", "5"])
+
+  name          = "my-bucket-${each.key}"
+  location      = "US"
+  storage_class = "STANDARD"
+  force_destroy = true
+}
