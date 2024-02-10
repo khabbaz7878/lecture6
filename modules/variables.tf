8bf67ddad0e52eb44bd79e786effd58d6d3e3b04
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-09 20:30:05 -0500

    Delete modules directory

--- modules/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "bucket_name" {
-  description = "Name for the storage bucket"
-}
-
-variable "location" {
-  description = "Location for the storage bucket"
-  default     = "us-central1"
-}
