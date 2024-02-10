5be27ab713b29b2867b4236ece20efee1765a227
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-09 20:27:09 -0500

    Create variables.tf

---
+++ modules/variables.tf
@@ -0,0 +1,8 @@
+variable "bucket_name" {
+  description = "Name for the storage bucket"
+}
+
+variable "location" {
+  description = "Location for the storage bucket"
+  default     = "us-central1"
+}
