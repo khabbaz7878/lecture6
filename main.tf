81a5de4e03a7c2cd20aa836cae7478acc349bebc
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-12 19:15:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
-module "bucket" {
-  source = "./storage_bucket"
+# Define the Google Cloud provider
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
 
-  bucket_name = "my-bucket"
+# Create the Google Cloud Storage buckets
+resource "google_storage_bucket" "buckets" {
+  count       = length(var.bucket_names)
+  name        = var.bucket_names[count.index]
+  location    = var.region
 }
