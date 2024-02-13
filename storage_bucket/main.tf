03883c1498125903ba799e514ea9effc20c20965
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-12 19:15:18 -0500

    Update main.tf

--- storage_bucket/main.tf
+++ storage_bucket/main.tf
@@ -1,13 +1,13 @@
-# storage_bucket/main.tf
-
-# Define bucket resource
-resource "google_storage_bucket" "bucket" {
-  name          = var.bucket_name
-  location      = var.location
-  storage_class = var.storage_class
-  force_destroy = true
+provider "google" {
+  project = "speedy-toolbox-360600"
+  region  = "us-central1"
 }
 
-
+module storage_bucket {
+  source       = "./storage_bucket"
+  project_id   = "speedy-toolbox-360600"
+  region       = "us-central1"
+  bucket_names = ["bucket1", "bucket2", "bucket3", "bucket4", "bucket5"]
+}
 
 
